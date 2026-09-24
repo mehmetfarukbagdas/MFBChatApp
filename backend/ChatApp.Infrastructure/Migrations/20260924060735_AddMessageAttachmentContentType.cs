7110d615548ff555dabe56dc7bb7165bfc523398
Mehmet Faruk Bağdaş <mehmetfarukbagdas@gmail.com>
2026-09-24 09:09:31 +0300

    Add message attachment content type migration

---
+++ backend/ChatApp.Infrastructure/Migrations/20260924060735_AddMessageAttachmentContentType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessageAttachmentContentType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
